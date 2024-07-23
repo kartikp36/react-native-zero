9c1822a49f17a2246f088bb669d78957ee817441
kartikp36 <kartikp2002@gmail.com>
2024-07-24 03:49:02 +0530

    feat: add nativewind🎨

--- app/index.tsx
+++ app/index.tsx
@@ -4,13 +4,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex flex-1 gap-4 flex-col items-center justify-center bg-blue-50"
     >
-      <Text>Aora!</Text>
+      <Text className="text-2xl">Aora!</Text>
       <Link href={'https://youtu.be/ZBCUegTZF7M?si=8m_nH659BVq5bVCb'} style={{color: 'blue', textDecorationLine: 'underline'}}>Javascript Mastery</Link>
       <Link href={'/profile'} style={{color: 'navy', textDecorationLine: 'underline'}}>Profile</Link>
     </View>
