e2f0ceb813882736ba7e80a11bce5c1ec0a8bf56
kartikp36 <kartikp2002@gmail.com>
2024-09-11 01:42:08 +0530

    fix: add tailwind styles for aora

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex flex-1 gap-4 flex-col items-center justify-center bg-blue-50">
-      <Text className="text-2xl">Aora!</Text>
+      <Text className="text-2xl text-primary">Aora!</Text>
       <Link
         href={"https://youtu.be/ZBCUegTZF7M?si=8m_nH659BVq5bVCb"}
         style={{ color: "blue", textDecorationLine: "underline" }}
@@ -13,7 +13,7 @@ export default function Index() {
       </Link>
       <Link
         href={"/profile"}
-        style={{ color: "navy", textDecorationLine: "underline" }}
+        className="text-blue-500 underline"
       >
         Profile
       </Link>
